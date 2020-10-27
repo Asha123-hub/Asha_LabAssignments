f2fccdbada3a9955c46b8e30fd64e0dc6a5730a4
Asha123-hub <73278291+Asha123-hub@users.noreply.github.com>
2020-10-27 21:07:03 +0530

    Add files via upload

---
+++ Lab3_Assignment/src/PositiveString.java
@@ -0,0 +1,33 @@
+/**
+ * Desc    - To check whether the given string is a positive string.
+ * @author - Asha Vinod
+ * Date    - 26-10-2020
+ *
+ */
+import java.util.Scanner;
+public class PositiveString { 
+	public static boolean checkPositiveString(String str) {    // A method that accepts a String and checks if it is a positive string
+		int length=str.length();
+		for(int outerIndex=0;outerIndex<length;outerIndex++) {
+			char ch=str.charAt(outerIndex);
+			for(int innerIndex=outerIndex+1;innerIndex<length;innerIndex++) {
+				if(ch<str.charAt(innerIndex)) {
+					continue;
+				}
+				else
+					return false;
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+        System.out.println("Enter the input string : ");
+        String inputStr=scan.next();
+        boolean result=checkPositiveString(inputStr);
+        System.out.println(result);
+        scan.close();
+	}
+
+}
