e049ad853b08b07a6fc160c69a732e26d0612dee
Asha123-hub <73278291+Asha123-hub@users.noreply.github.com>
2020-11-02 21:48:41 +0530

    Add files via upload

---
+++ Lab4_Assignment/src/bank_account/Account.java
@@ -0,0 +1,67 @@
+/**
+ * Author : Asha Vinod
+ * Date   : 02-11-2020
+ */
+package bank_account;
+
+public class Account {
+	private long accNum;
+	private double balance;
+	private Person accHolder;
+
+	/**
+	 * Parameterized constructor
+	 */
+
+	public Account(long accNum, double balance, Person accHolder) {
+		this.balance = balance;
+		this.accHolder = accHolder;
+		this.accNum = accNum;
+	}
+
+	/**
+	 * Getters and setters for the account element
+	 */
+
+	public long getAccNum() {
+		return accNum;
+	}
+
+	public void setAccNum(long accNum) {
+		this.accNum = accNum;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public Person getAccHolder() {
+		return accHolder;
+	}
+
+	public void setAccHolder(Person accHolder) {
+		this.accHolder = accHolder;
+	}
+
+	/**
+	 * This method performs deposit operation
+	 */
+
+	public void deposit(double amount) {
+		balance = balance + amount;
+
+	}
+
+	/**
+	 * This method performs withdrawal operation
+	 */
+
+	public void withdraw(double amount) {
+		balance = balance - amount;
+
+	}
+}
---
+++ Lab4_Assignment/src/bank_account/Person.java
@@ -0,0 +1,43 @@
+/**
+ * Author : Asha Vinod
+ * Date   : 02-11-2020
+ */
+package bank_account;
+
+public class Person {
+	private String name;
+	private float age;
+
+	/**
+	 * Parameterized Constructor
+	 * 
+	 * @param name
+	 * @param age
+	 */
+	public Person(String name, float age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+	/**
+	 * Getters and setters for the account element
+	 */
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getAge() {
+		return age;
+	}
+
+	public void setAge(float age) {
+		this.age = age;
+	}
+
+}
